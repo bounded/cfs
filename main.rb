5a3d56d63e214e9d83455e2a71024cf3c9f6580a
Maxim Schmidt <maxim.schmidt42@googlemail.com>
2014-01-18 21:00:26 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,25 @@
+require './cfs.rb'
+
+input = File.read "input"
+db = CFS::Database.new
+
+input.split("\n").map{|x| x.strip}.delete_if{|x| x.empty?}.each {|l|
+  db.add (CFS::Parser::parse_l l)
+}
+
+puts "#" * 8
+puts "Database:"
+puts db.to_s
+puts "#" * 8
+puts "Enter filter:"
+
+while f = gets
+  break unless f
+
+  f.chomp!
+  f.strip!
+
+  puts (db.filter (CFS::Parser::parse_cs f)).to_s
+  puts "#" * 8
+  puts "Enter filter:"
+end
